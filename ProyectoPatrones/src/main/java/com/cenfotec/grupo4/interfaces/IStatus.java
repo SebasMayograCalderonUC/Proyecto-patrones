ef802e995b65d011a6cd4923f8bed2549aed1452
Sebastian mayorga <smayorgac@ucenfotec.ac.cr>
2017-11-28 17:14:18 -0600

    commit 28/11/17

--- ProyectoPatrones/src/main/java/com/cenfotec/grupo4/interfaces/IStatus.java
+++ ProyectoPatrones/src/main/java/com/cenfotec/grupo4/interfaces/IStatus.java
@@ -1,10 +1,22 @@
 package com.cenfotec.grupo4.interfaces;
 
-import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.annotate.JsonSubTypes;
+import org.codehaus.jackson.annotate.JsonTypeInfo;
 
 import com.cenfotec.grupo4.entities.Procedure;
-@JsonSerialize
+import com.cenfotec.grupo4.entities.Task;
+import com.cenfotec.grupo4.interfaces.users.*;
+
+@JsonTypeInfo(use=JsonTypeInfo.Id.NAME,
+include=JsonTypeInfo.As.PROPERTY,
+property="name")
+
+@JsonSubTypes({
+    @JsonSubTypes.Type(value=StatusActive.class, name="Active"),
+    @JsonSubTypes.Type(value=StatusFinalized.class, name="Finalized"),
+})
 public interface IStatus {
-	public String treatProcedure(Procedure procedure,boolean desition);
+	public String treatTask(Procedure procedure,boolean desition);
+	public boolean CheckTask(Procedure procedure);
 	public void changeProcedureStatus(Procedure procedure);
 }
--- ProyectoPatrones/src/main/java/com/cenfotec/grupo4/interfaces/users/StatusFinalized.java
+++ ProyectoPatrones/src/main/java/com/cenfotec/grupo4/interfaces/users/StatusFinalized.java
@@ -1,8 +1,13 @@
 package com.cenfotec.grupo4.interfaces.users;
 
+import org.codehaus.jackson.annotate.JsonTypeName;
+
 import com.cenfotec.grupo4.entities.Procedure;
+import com.cenfotec.grupo4.entities.Task;
 import com.cenfotec.grupo4.interfaces.IStatus;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonTypeName("Finalized")
 
 public class StatusFinalized implements IStatus{
 	@JsonProperty
@@ -11,21 +16,30 @@ public class StatusFinalized implements IStatus{
 	public StatusFinalized() {
 		status="Finalized";
 	}
-	public StatusFinalized(String status) {
-		this.status=status;
-	}
 
 	public String getStatus() {
 		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
 	}
 
 	public void changeProcedureStatus(Procedure procedure) {
 		// TODO Auto-generated method stub
 		
 	}
+
 	@Override
-	public String treatProcedure(Procedure procedure, boolean desition) {
+	public String treatTask(Procedure procedure, boolean desition) {
+		// TODO Auto-generated method stub
 		return "Procedure has ended";
+	}
+
+	@Override
+	public boolean CheckTask(Procedure procedure) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
