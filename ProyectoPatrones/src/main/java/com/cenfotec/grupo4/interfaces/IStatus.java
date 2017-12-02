5fb7320a34175e7df3bc55c6acd655b22de87da8
Sebastian mayorga <smayorgac@ucenfotec.ac.cr>
2017-12-02 09:53:57 -0600

    lol

--- ProyectoPatrones/src/main/java/com/cenfotec/grupo4/interfaces/IStatus.java
+++ ProyectoPatrones/src/main/java/com/cenfotec/grupo4/interfaces/IStatus.java
@@ -5,8 +5,8 @@ import org.codehaus.jackson.annotate.JsonTypeInfo;
 
 import com.cenfotec.grupo4.entities.Procedure;
 import com.cenfotec.grupo4.entities.Task;
-import com.cenfotec.grupo4.interfaces.users.StatusActive;
-import com.cenfotec.grupo4.interfaces.users.StatusFinalized;
+import com.cenfotec.grupo4.interfaces.use.StatusActive;
+import com.cenfotec.grupo4.interfaces.use.StatusFinalized;
 
 
 @JsonTypeInfo(use=JsonTypeInfo.Id.NAME,
