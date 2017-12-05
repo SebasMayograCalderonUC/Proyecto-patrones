68bb6af6f872af7fecf765c060d43feaa7a37266
Sebastian mayorga <smayorgac@ucenfotec.ac.cr>
2017-12-04 21:56:01 -0600

    s

--- ProyectoPatrones/src/main/java/com/cenfotec/grupo4/main/ProyectoPatronesApplication.java
+++ ProyectoPatrones/src/main/java/com/cenfotec/grupo4/main/ProyectoPatronesApplication.java
@@ -29,6 +29,12 @@ import com.cenfotec.grupo4.entities.Department;
 import com.cenfotec.grupo4.entities.Employee;
 import com.cenfotec.grupo4.entities.Procedure;
 import com.cenfotec.grupo4.entities.Task;
+
+import com.cenfotec.grupo4.gestor.Gestor;
+import com.cenfotec.grupo4.utils.JsonManager;
+import com.cenfotec.grupo4.utils.encrypt.EncryptManagerSymetric;
+import com.cenfotec.grupo4.utils.encrypt.Encryptor;
+
 import com.cenfotec.grupo4.utils.*;
 import org.apache.bsf.util.event.adapters.java_awt_event_ActionAdapter;
 import org.codehaus.*;
