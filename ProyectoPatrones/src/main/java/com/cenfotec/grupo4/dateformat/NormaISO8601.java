a0f1368b63e704ea4890ff5f0df46dd9f6886586
MarvinJesus <mar90jesus@gmail.com>
2017-12-08 11:47:10 -0600

    Update CalendarCustom
    Update CalendarCustom

--- ProyectoPatrones/src/main/java/com/cenfotec/grupo4/dateformat/NormaISO8601.java
+++ ProyectoPatrones/src/main/java/com/cenfotec/grupo4/dateformat/NormaISO8601.java
@@ -8,7 +8,7 @@ public class NormaISO8601 implements com.cenfotec.grupo4.dateformat.DateFormat{
 	@Override
 	public String getFormattedDate() {
 		Date date = new Date();
-		DateFormat fechaHora = new SimpleDateFormat("YYYY-MM-DD");
+		DateFormat fechaHora = new SimpleDateFormat("YYYY-MM-dd");
 		String dateFormat = fechaHora.format(date);
 		return dateFormat;
 	}
