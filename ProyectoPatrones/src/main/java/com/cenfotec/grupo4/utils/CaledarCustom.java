e0162ef26d9c0d86d049f5a205c66c07d6729e1d
MarvinJesus <mar90jesus@gmail.com>
2017-12-08 11:26:16 -0600

    Update Code DateFormat
    Update Code DateFormat

--- ProyectoPatrones/src/main/java/com/cenfotec/grupo4/utils/CaledarCustom.java
+++ ProyectoPatrones/src/main/java/com/cenfotec/grupo4/utils/CaledarCustom.java
@@ -4,9 +4,10 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class CaledarCustom {
-	Date date = new Date();
-	DateFormat fechaHora = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	String convertido = fechaHora.format(date);
-
+public class CaledarCustom implements com.cenfotec.grupo4.dateformat.DateFormat{
+	@Override
+	public String getFormattedDate() {
+		
+		return new ;
+	}
 }
