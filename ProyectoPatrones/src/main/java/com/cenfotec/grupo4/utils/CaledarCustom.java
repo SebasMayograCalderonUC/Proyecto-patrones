a2cc03fada419db5bb72318d1b88bf1de2e9719b
MarvinJesus <mar90jesus@gmail.com>
2017-12-08 11:29:22 -0600

    Update Code
    Update Code

--- ProyectoPatrones/src/main/java/com/cenfotec/grupo4/utils/CaledarCustom.java
+++ ProyectoPatrones/src/main/java/com/cenfotec/grupo4/utils/CaledarCustom.java
@@ -4,10 +4,12 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import com.cenfotec.grupo4.dateformat.NormaISO8601;
+
 public class CaledarCustom implements com.cenfotec.grupo4.dateformat.DateFormat{
 	@Override
 	public String getFormattedDate() {
 		
-		return new ;
+		return new NormaISO8601().getFormattedDate();
 	}
 }
