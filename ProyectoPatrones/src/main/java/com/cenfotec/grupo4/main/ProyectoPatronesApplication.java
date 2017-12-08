874b1a64a7c20fa8064a3957d2f596943ee09750
JoseRamirezO <jramirezo@ucenfotec.ac.cr>
2017-12-08 11:23:49 -0600

    Merge master

--- ProyectoPatrones/src/main/java/com/cenfotec/grupo4/main/ProyectoPatronesApplication.java
+++ ProyectoPatrones/src/main/java/com/cenfotec/grupo4/main/ProyectoPatronesApplication.java
@@ -1,62 +1,15 @@
 package com.cenfotec.grupo4.main;
 
-<<<<<<< HEAD
-=======
-import java.awt.Point;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.lang.reflect.Array;
-import java.text.DateFormat;
-
-import java.text.SimpleDateFormat;
-
-import java.text.SimpleDateFormat;
-
-import java.util.ArrayList;
-import java.util.Date;
-
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.annotate.JsonMethod;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.*;
-import org.mockito.internal.matchers.VarargCapturingMatcher;
-
->>>>>>> master
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-<<<<<<< HEAD
 import com.cenfotec.grupo4.gestor.Action;
-=======
-import com.cenfotec.grupo4.entities.Department;
-import com.cenfotec.grupo4.entities.Employee;
-import com.cenfotec.grupo4.entities.Procedure;
-import com.cenfotec.grupo4.entities.Task;
-
 import com.cenfotec.grupo4.gestor.GestorProcess;
-import com.cenfotec.grupo4.gestor.Action;
 import com.cenfotec.grupo4.utils.GestorCreate;
-import com.cenfotec.grupo4.utils.JsonManager;
-import com.cenfotec.grupo4.utils.encrypt.EncryptManagerSymetric;
-import com.cenfotec.grupo4.utils.encrypt.Encryptor;
-
-
-import com.cenfotec.grupo4.utils.*;
-import org.apache.bsf.util.event.adapters.java_awt_event_ActionAdapter;
-
-import org.codehaus.*;
-import org.springframework.boot.autoconfigure.jackson.*;
->>>>>>> master
 import org.springframework.core.env.Environment;
 
 
@@ -76,10 +29,7 @@ public class ProyectoPatronesApplication implements CommandLineRunner {
 	@Autowired
 	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	public void run(String... args) throws Exception {
-<<<<<<< HEAD
 
-	
-=======
 		ShowMessage("\n");
 		ShowMessage("\n");
 		ShowMessage("Controlador de Tramite  0.1");
@@ -90,7 +40,6 @@ public class ProyectoPatronesApplication implements CommandLineRunner {
         }while(!execMenu(option));
 		ShowMessage("\n");
 		ShowMessage("\n");
->>>>>>> master
     }
 	public boolean execMenu(int opcion) {
 		try {
@@ -116,12 +65,10 @@ public class ProyectoPatronesApplication implements CommandLineRunner {
 		}
 		return false;
 	}
-<<<<<<< HEAD
 	public void createProcedure() {
 		String procedureNam;
 	}
-}
-=======
+	
 	public static int GetInteger(int limit) {
 	   int  option ;
 	   do {   
@@ -155,11 +102,6 @@ public class ProyectoPatronesApplication implements CommandLineRunner {
 }
 
 
- 
-
-	
-
->>>>>>> master
 
 
 
