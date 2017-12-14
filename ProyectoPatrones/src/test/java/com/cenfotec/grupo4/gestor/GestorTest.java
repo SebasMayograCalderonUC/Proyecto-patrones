7b3b46dd9de704026e4b6bf98e3c5d61231c7e0a
BrandonGonzalezA <killerp8420@gmail.com>
2017-12-14 16:07:11 -0600

    GestorGeneralTestUpdateCode

---
+++ ProyectoPatrones/src/test/java/com/cenfotec/grupo4/gestor/GestorTest.java
@@ -0,0 +1,60 @@
+package com.cenfotec.grupo4.gestor;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.cenfotec.grupo4.entities.Department;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GestorTest {
+	private static GestorGeneral gestor;
+	
+	@BeforeClass
+	public static void init() throws JsonParseException, JsonMappingException, IOException {
+		gestor = new GestorGeneral();
+	}
+	
+	@Test
+	public static GestorGeneral getInstance() throws JsonParseException, JsonMappingException, IOException {
+		return gestor.getInstance();
+	}
+	@Test
+	public ArrayList<Department> getDepartments(){
+		return gestor.getDepartments();
+	}
+	@Test
+	public String obtenerProcedimiento() throws Exception {
+		return gestor.obtenerProcedimiento();
+	}
+	@Test
+	public String enviarProcedimiento() throws JsonGenerationException, JsonMappingException, IOException, Exception {
+		return gestor.enviarProcedimiento(0, null);
+	}
+	@Test
+	public String tratarProcedimiento() {
+		return gestor.tratarProcedimiento(0, true);
+	}
+	@Test
+	public String CrearProcedimiento() {
+		return gestor.CrearProcedimiento(null, null);
+	}
+	@Test
+	public String getAllActiveProcedures() {
+		return gestor.getAllActiveProcedures();
+	}
+	@Test
+	public String getAllFinalizedProcedures() {
+		return gestor.getAllFinalizedProcedures();
+	}
+}
